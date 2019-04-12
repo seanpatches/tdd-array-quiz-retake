46f9879942454faba14ac29ca8371f89619851f8
Sean Nelson <smnelsonn@gmail.com>
2019-04-12 15:19:35 -0700

    bonus tests

--- test/index.test.js
+++ test/index.test.js
@@ -8,4 +8,17 @@ describe('quiz function', () =>{
     expect(bigFinder(array)).toEqual(['Wow, 7 is big!']);
       
   });
+
+  it('works with a my numbers', () => {
+    let array = [10, 3, 5, 7, 2];
+
+    expect(bigFinder(array)).toEqual(['Wow, 10 is big!', 'Wow, 7 is big!']);
+  });
+
+  it('works with a my numbers', () => {
+    let array = [10, 3, 11, 5, 7, 2];
+
+    expect(bigFinder(array)).toEqual(['Wow, 10 is big!', 'Wow, 11 is big!', 'Wow, 7 is big!']);
+  });
+
 });
